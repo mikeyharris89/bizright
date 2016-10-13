31f30095f4e29a692f8f3b2e7bb631c3da6dbbc1
Mikey Harris <mikey.harris89@gmail.com>
2016-10-13 12:32:59 -0400

    Questions flow implemented

--- js/header_animations.js
+++ js/header_animations.js
@@ -1,14 +1,44 @@
 (function() {
 
-  var ctaButton = $('button.btn-cta');
+  var $ctaButton = $('button.btn-cta');
 
-  ctaButton.click(function (e) {
+  $ctaButton.click(function (e) {
     e.preventDefault();
     changeText();
   })
   var changeText = function() {
-    console.log('hey');
-    $('.heading-2').text("Great. You're on the right track \n Just a few questions..");
+    $('.bullet2:first-child').text("Great. You're now on the right track");
+    $('.bullet2:last-child').text("Just a few questions..");
+    addBox();
   }
 
+  var addBox = function() {
+    $('button.btn-cta').remove()
+    var $infoBox = $('<div>').addClass('info-box'),
+        $instruction = $('<p>').text("Step One: Choose Your Business"),
+        $input = $('<input>').attr("placeholder", "Business Name"),
+        $nextButton = $('<button>').addClass('btn-secondary').text('Continue');
+
+    $infoBox.append($instruction, $input, $nextButton);
+    $('.landing-flow').append($infoBox);
+
+    $nextButton.click( function(e) {
+      e.preventDefault();
+      updateBox($instruction, $nextButton, $input);
+    })
+  }
+
+  var updateBox = function($instruction, $nextButton, $input, e) {
+    // e.preventDefault();
+    $instruction.text("Where dat business at");
+    $input.remove();
+    $stateBar = $('select#state');
+
+    $('.info-box').append($stateBar.show());
+    $nextButton.text("See The Score");
+  }
+
+  // var makeState = function() {
+  //   $stateBar = $('<select>').addClass("form-control input-lg").attr('id', );
+  // }
 }(jQuery, _));
