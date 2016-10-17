c4b27a1497d393edbd16030e218f38f8ca8ff7cc
Mikey Harris <mikey.harris89@gmail.com>
2016-10-17 18:03:19 -0400

    finetune animations

--- js/header_animations.js
+++ js/header_animations.js
@@ -10,41 +10,41 @@
   window.setTimeout(function(){
     $('.heading-1').css({display: "none"});
     changeText();
-  }, 2500)
+  }, 1500)
   $ctaButton.click(function (e) {
     e.preventDefault();
     addBox();
   })
 
   var addBox = function() {
-    $firstBullet = $('.bullet:first-child').text("Great You're now on the right track!");
+    $firstBullet = $('.bullet:first-child').text("Great, You're now on the right track!");
     $underline = $('<div>').addClass('underline');
+    $searchBar = $('<div>').addClass('search-bar');
+
     $firstBullet.append($underline);
-    $('.bullet:last-child').text("Just a few questions..");
-    // $('.heading-1').removeClass("heading-1").addClass('new-heading');
+    $('.bullet:last-child').text("Let's get started..");
     $('.heading-1').css({display: "none"}).fadeIn("slow");;
     $('button.btn-cta').remove()
     var $infoBox = $('<div>').addClass('info-box'),
         $instruction = $('<p>').text("Step One: Choose Your Business"),
-        $input = $('<input>').attr("placeholder", "Business Name"),
-        $nextButton = $('<button>').addClass('btn-secondary').text('Continue');
+        $input = $('<input>').attr("placeholder", "Business Name").addClass('search-input col-sm-4'),
+        $stateBar = $('select#state').css({display: "block"}).addClass('col-sm-4');
+        $nextButton = $('<button>').addClass('btn-secondary btn-search col-sm-4').text('See The Score');
 
-    $infoBox.append($instruction, $input, $nextButton);
+    $searchBar.append($input, $stateBar, $nextButton);
+    $infoBox.append($instruction, $searchBar);
     $('.landing-flow').append($infoBox);
 
-    $nextButton.click( function(e) {
-      e.preventDefault();
-      updateBox($instruction, $nextButton, $input);
-    })
+    // $nextButton.click( function(e) {
+    //   e.preventDefault();
+    //   updateBox($instruction, $nextButton, $input);
+    // })
   }
 
   var updateBox = function($instruction, $nextButton, $input, e) {
     $instruction.text("Where dat business at").addClass('box-header');
     $input.remove();
-    $stateBar = $('select#state');
 
-    $stateBar.insertAfter('.box-header');
-    $stateBar.css({display:"block"});
     $nextButton.text("See The Score");
   }
 
